3badcd95f792784aebf004b782ce992cfd9b9c58
Koen <hicswk7278@clarkstate.edu>
2024-03-21 16:27:46 -0400

    Fixes termination errors

--- uninstaller/uninstaller/uninstaller.cpp
+++ uninstaller/uninstaller/uninstaller.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib> // For system
+#include <cstdio> // For remove
 
 int main()
 {
 	//Terminate the tasks first
-	system("taskkill /f /im ExternalCrossHairOverlay.exe");
-	system("taskkill /f /im Lightning Macros.exe");
-	system("taskkill /f /im Lightning Launcher.exe");
+   // Kill processes
+    system("taskkill /f /im ExternalCrossHairOverlay.exe");
+    system("taskkill /f /im \"Lightning Macros.exe\"");
+    system("taskkill /f /im \"Lightning Launcher.exe\"");
 
-	remove(".\\ExternalCrossHairOverlay.exe");
-	remove(".\\Lightning Macros.exe");
-	remove(".\\Lightning Launcher.exe");
+    // Remove files
+    remove(".\\ExternalCrossHairOverlay.exe");
+    remove(".\\Lightning Macros.exe");
+    remove(".\\Lightning Launcher.exe");
+
 	std::cout << "Attempted to remove files\n";
 	std::cin.get();
 }
